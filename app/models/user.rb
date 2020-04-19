166bb19adc8338c0a146f0a46e49d1ce9aac3b58
takafumi-yoshida <59679612+takafumi-yoshida@users.noreply.github.com>
2020-04-19 16:22:03 +0900

    Userモデル validates

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,17 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
          
    mount_uploader :image, ImageUploader
+
+   
+   with_options presence: true do
+    validates :artistname, uniqueness: true, length: { maximum: 10 }
+    validates :email, uniqueness: true 
+    validates :encrypted_password
+  end
+
+    validates :realname,       length: { maximum: 18 }
+    validates :age,            length: { maximum: 3 }, format: {with: /\A[0-9]{3}\z/}
+    validates :profession,     length: { maximum: 12 }
+    validates :birthplace,     length: { maximum: 12 }
+    validates :activityBase,   length: { maximum: 12 }
 end
