500121fe6fdeaef851fc74de7a9ccfd22e0f8c59
takafumi-yoshida <59679612+takafumi-yoshida@users.noreply.github.com>
2020-05-14 13:56:52 +0900

    update (gallery show)

--- app/assets/javascripts/channels/gallery/show.js
+++ app/assets/javascripts/channels/gallery/show.js
@@ -2,7 +2,7 @@ $(function(){
   let right = $(".gallery_information__right");
   let btn = $(".gallery_information__btn");
   let rightHeight = $(".gallery_information__right").height();
-  let index = 0
+  let index = false
 
   if(rightHeight > 160) {
     right.height(140);
@@ -10,18 +10,18 @@ $(function(){
   };
 
   btn.click(function(){
-    if(index == 0) {
+    if(index == false) {
       right.height(rightHeight);
       right.hide();
       right.slideToggle(500);
       btn.text('説明文を閉じる');
-      index = 1;
+      index = true;
     } else {
       right.slideToggle(0);
       right.height(140);
       right.show(500);
       btn.text('全文を表示する');
-      index = 0;
+      index = false;
     }
   });
 });
