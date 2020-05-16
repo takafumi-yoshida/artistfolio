df76c355b0c19c0630b8b59d7bc991ae283245eb
takafumi-yoshida <59679612+takafumi-yoshida@users.noreply.github.com>
2020-05-16 17:13:59 +0900

    add(meta-tags)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,26 @@
 module ApplicationHelper
+  def default_meta_tags
+    {
+      site: 'Artistfolio',
+      title: 'アーティストフォリオ',
+      reverse: true,
+      charset: 'utf-8',
+      description: 'Art（只今、写真のみ）の投稿、閲覧サービス。写真集を作ることもでき、スライドショーで楽しむことができます。また、ポートフォリオとしてもお使いいただけます。',
+      keywords: 'Art,Artist,Photo,Gallery',
+      canonical: request.original_url,
+      separator: '|',
+      icon: [
+        { href: image_url('/favicon.ico') },
+      ],
+      og: {
+        site_name: 'Artistfolio',
+        title: 'アーティストフォリオ',
+        description: 'Art（只今、写真のみ）の投稿、閲覧サービス。写真集を作ることもでき、スライドショーで楽しむことができます。また、ポートフォリオとしてもお使いいただけます。', 
+        type: 'website',
+        url: request.original_url,
+        image: image_url('/sample.png'),
+        locale: 'ja_JP',
+      }
+    }
+  end
 end
