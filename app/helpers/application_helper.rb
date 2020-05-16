0e56e5b3a8540a63810ddc4107aed24679687cc0
takafumi-yoshida <59679612+takafumi-yoshida@users.noreply.github.com>
2020-05-16 17:54:57 +0900

    update(meta_tags)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,7 +5,7 @@ module ApplicationHelper
       title: 'アーティストフォリオ',
       reverse: false,
       charset: 'utf-8',
-      description: 'Art（只今、写真のみ）の閲覧、投稿サービス。写真集を作ることもでき、スライドショーで楽しむことができます。また、ポートフォリオとしてもお使いいただけます。あなたも自分の作った作品アップロードして、世界中のみんなに共有してみませんか？',
+      description: 'アート（只今、写真のみ）の閲覧、投稿サービス。写真集を作ることもでき、スライドショーで楽しむことができます。また、ポートフォリオとしてもお使いいただけます。あなたも自分の作った作品アップロードして、世界中のみんなに共有してみませんか？',
       keywords: 'Art,Artist,Photo,Gallery',
       canonical: request.original_url,
       separator: '|',
@@ -15,7 +15,7 @@ module ApplicationHelper
       og: {
         site_name: 'Artistfolio',
         title: 'アーティストフォリオ',
-        description: 'Art（只今、写真のみ）の閲覧、投稿サービス。写真集を作ることもできki、スライドショーで楽しむことができます。また、ポートフォリオとしてもお使いいただけます。あなたも自分の作った作品アップロードして、世界中のみんなに共有してみませんか？', 
+        description: 'アート（只今、写真のみ）の閲覧、投稿サービス。写真集を作ることもでき、スライドショーで楽しむことができます。また、ポートフォリオとしてもお使いいただけます。あなたも自分の作った作品アップロードして、世界中のみんなに共有してみませんか？', 
         type: 'website',
         url: request.original_url,
         image: image_url('/sample.png'),
