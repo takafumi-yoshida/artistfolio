c31ef4af54481ff68d24641281afc937709bdb8f
takafumi-yoshida <59679612+takafumi-yoshida@users.noreply.github.com>
2020-05-16 18:24:56 +0900

    debug(meta_tags)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,11 @@
 module ApplicationHelper
   def default_meta_tags
     {
-      site: site.meta_tag.name,
-      title: site.meta_tag.title,
+      site: "Artistfolio",
+      title: "アーティストフォリオ",
       reverse: false,
       charset: 'utf-8',
-      description: site.meta_tag.description,
+      description: "アート作品（只今、写真のみ）の閲覧、投稿サービス。写真集を作ることもでき、スライドショーで楽しむことができます。また、ポートフォリオとしてもお使いいただけます。あなたも作品をアップロードして、世界中のみんなに共有してみませんか？",
       keywords: 'Art,Artist,Photo,Gallery',
       canonical: request.original_url,
       separator: '|',
@@ -13,9 +13,9 @@ module ApplicationHelper
         { href: image_url('/favicon.ico') },
       ],
       og: {
-        site_name: site.meta_tag.name,
-        title: site.meta_tag.title,
-        description: site.meta_tag.description, 
+        site_name: "Artistfolio",
+        title: "アーティストフォリオ",
+        description: "アート作品（只今、写真のみ）の閲覧、投稿サービス。写真集を作ることもでき、スライドショーで楽しむことができます。また、ポートフォリオとしてもお使いいただけます。あなたも作品をアップロードして、世界中のみんなに共有してみませんか？", 
         type: 'website',
         url: request.original_url,
         image: image_url('/sample.png'),
